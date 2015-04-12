aaba96beb857b8d1d06ef9bb14d1de400087c79b
akrigline <andrew@akrigline.com>
2015-04-12 18:50:56 -0400

    Hover works, applying delays

--- js/krigline-imageSwap.js
+++ js/krigline-imageSwap.js
@@ -1,37 +1,25 @@
+$imgSwap = $('.demo-hover'); //Class applied to all image swap triggers
 
-
-$imgHovered = $('.demo-hover');
-
-/*
-$imgHovered.hover(
-  function(){
-    console.log('hovered!')
-  }, function(){
-    console.log('unHovered!')
-  }
+$imgSwap.hover( 
+  changeSrc,    // mouseenter: Execute the changeSrc function below
+  changeBack    // mouseleave: Execute the changeBack function below
 );
-*/
-$imgHovered.hover( changeSrc, changeBack );
 
 function changeSrc() {
-  var collection = $(this).attr('data-collection');
-  var demoSrc = $(this).attr('data-demo');
-
-  
-
-  $demoImg = $('#' + collection);
+  var collection = $(this).attr('data-collection');   //Get the "Collection" of the trigger
+  $demoImg = $('#' + collection);                     //Find the placeholder applied to that collection
+  var demoSrc = $(this).attr('data-demo');            //Get the image applied to the trigger
 
   $demoImg.attr('src', demoSrc);
 
 }
 
 function changeBack() {
-  var collection = $(this).attr('data-collection');
+  var collection = $(this).attr('data-collection');   //Get the "Collection" of the trigger
+  $demoImg = $('#' + collection);                     //Find the placeholder applied to that collection
+  var demoOriginal = $demoImg.attr('data-original');  //Get the original image for the placeholder
 
-  $demoImg = $('#' + collection);
-
-  var demoOriginal = $demoImg.attr('data-original');
-
-  $demoImg.attr('src', demoOriginal);
-
+  setTimeout(function(){
+    $demoImg.attr('src', demoOriginal);
+  }, 2500);
 }
