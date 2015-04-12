8ecd5df20ba1113a44c15f01517478a08b604f5b
akrigline <andrew@akrigline.com>
2015-04-12 19:13:00 -0400

    applying more documentation and transitions

--- js/krigline-imageSwap.js
+++ js/krigline-imageSwap.js
@@ -1,5 +1,8 @@
 $imgSwap = $('.demo-hover'); //Class applied to all image swap triggers
 
+//Declare the timer exists as a variable outside the function that it's created in (scope) so that it can be modified outside that function
+var timer;
+
 $imgSwap.hover( 
   changeSrc,    // mouseenter: Execute the changeSrc function below
   changeBack    // mouseleave: Execute the changeBack function below
@@ -10,8 +13,9 @@ function changeSrc() {
   $demoImg = $('#' + collection);                     //Find the placeholder applied to that collection
   var demoSrc = $(this).attr('data-demo');            //Get the image applied to the trigger
 
-  $demoImg.attr('src', demoSrc);
-
+  clearTimeout(timer);                                //Stop the Timeout caused by mouseleave of a trigger
+  $demoImg.attr('src', demoSrc);                      //Apply the image to the placeholder
+  $(this).addClass('demo-active');
 }
 
 function changeBack() {
@@ -19,7 +23,12 @@ function changeBack() {
   $demoImg = $('#' + collection);                     //Find the placeholder applied to that collection
   var demoOriginal = $demoImg.attr('data-original');  //Get the original image for the placeholder
 
-  setTimeout(function(){
-    $demoImg.attr('src', demoOriginal);
-  }, 2500);
+  //We don't want the image to immediately revert, so we create a timer that can be cancelled
+  timer = setTimeout(function(){
+            $demoImg.attr('src', demoOriginal);       //Apply the original image to the placeholder again
+          }, 1500);
+  
+  //Remove that Active class since it's no longer true
+  $(this).removeClass('demo-active');
+
 }
